4f38c4b1e39330f6dad1ac36f72e0e41bdb715b1
UBauerle <ubauerle@gmail.com>
2025-07-07 09:13:48 -0300

    Atualização

---
+++ vcl/uBiblioteca_old.pas
@@ -0,0 +1,498 @@
+unit uBiblioteca;
+
+interface
+
+Type
+  TRetorno = Record
+    Resultado: Boolean;
+    Mensagem: String;
+  End;
+
+  Function EmitirNFCeDePV(pSis_Pessoa_id, pCom_Pedido_Numero: Integer): TRetorno;
+  Function getValorParametro(pNome: String): String;
+
+implementation
+
+Uses uDados, FireDAC.Comp.Client, System.SysUtils, Xml.XMLDoc, Xml.XMLIntf,
+     Data.DB, Vcl.Dialogs;
+
+Var
+  fdqTemp1: TFDQuery;
+  vACNFE_HOME, vACNFE_EXE, vACNFE_TMP, vACNFE_PDF, vACNFE_XML, vACNFE_OUT: String;
+  vxmld: TXMLDocument;
+
+Procedure setfdqTemp1;
+Begin
+  if fdqTemp1 <> Nil then
+    Exit;
+
+  fdqTemp1:= TFDQuery.Create(uDM);
+  fdqTemp1.Connection:= uDM.FDC;
+
+End;
+
+
+Procedure CriarTabela_sis_parametros_se_necessario;
+Var
+  vExists, vCreateTable: String;
+Begin
+  setfdqTemp1;
+
+  Exit;
+  fdqTemp1.SQL.Text:=
+    'CALL sys.table_exists(''speedfood'', ''sis_parametros'', @exists); ' +
+    'SELECT @exists;';
+  fdqTemp1.Open;
+  vExists:= fdqTemp1.Fields[0].AsString;
+  fdqTemp1.Close;
+
+  If Not vExists.Trim.IsEmpty Then
+    Exit;
+
+  vCreateTable:=
+    'CREATE TABLE `speedfood`.`sis_parametros` ( ' +
+    '  `nome` VARCHAR(32) NOT NULL, ' +
+    '  `descricao` VARCHAR(120) NULL, ' +
+    '  `valor` VARCHAR(999) NULL, ' +
+    '  PRIMARY KEY (`nome`)); ';
+
+  fdqTemp1.ExecSQL(vCreateTable)
+
+End;
+
+Function getValorParametro(pNome: String): String;
+Begin
+
+  fdqTemp1.Close;
+  fdqTemp1.SQL.Text:=
+    'select valor ' +
+    'from sis_parametros ' +
+    'where nome = ' + pNome.QuotedString;
+  fdqTemp1.Open;
+
+  If fdqTemp1.IsEmpty Then
+    Result:= EmptyStr
+  Else
+    Result:= fdqTemp1.Fields[0].AsString;
+
+  fdqTemp1.Close;
+
+End;
+
+Function TestarSeValorParametroEstaVazio(pNomeParametro: String; Var pValorParametro: String): TRetorno;
+Begin
+
+  pValorParametro:= getValorParametro(pNomeParametro);
+  Result.Resultado:= Not pValorParametro.Trim.IsEmpty;
+
+  if Not Result.Resultado then
+  Begin
+    Result.Mensagem:= 'Parâmetro ' + pNomeParametro + ' encontra-se não informado ou vazio!';
+    Exit;
+  End;
+
+End;
+
+Function TestarSeValorParametroEhDiretorioExistente(pNomeParametro: String; pValorParametro: String): TRetorno;
+Begin
+
+  Result.Resultado:= DirectoryExists(pValorParametro);
+
+  if Not Result.Resultado then
+  Begin
+    Result.Mensagem:= 'Parâmetro ' + pNomeParametro + ', com valor ' + pValorParametro.QuotedString +  ' não representa um diretório existente!';
+    Exit;
+  End;
+
+End;
+
+Function TestarSeValorParametroEhArquivoExistente(pNomeParametro: String; pValorParametro: String): TRetorno;
+Begin
+
+  Result.Resultado:= System.SysUtils.FileExists(pValorParametro);
+
+  if Not Result.Resultado then
+  Begin
+    Result.Mensagem:= 'Parâmetro ' + pNomeParametro + ', com valor ' + pValorParametro.QuotedString +  ' não representa um arquivo existente!';
+    Exit;
+  End;
+
+End;
+
+Function TSVPE_VazioOuDir(pNomeParametro: String; Var pValorParametro: String): TRetorno;
+Begin
+  Result:= TestarSeValorParametroEstaVazio(pNomeParametro, pValorParametro);
+
+  if Not Result.Resultado then
+    Exit;
+
+  Result:= TestarSeValorParametroEhDiretorioExistente(pNomeParametro, pValorParametro);
+
+End;
+
+Function TSVPE_VazioOuArq(pNomeParametro: String; Var pValorParametro: String): TRetorno;
+Begin
+  Result:= TestarSeValorParametroEstaVazio(pNomeParametro, pValorParametro);
+
+  if Not Result.Resultado then
+    Exit;
+
+  Result:= TestarSeValorParametroEhArquivoExistente(pNomeParametro, pValorParametro);
+
+End;
+
+
+
+Function ValidarParametrosNFCe: TRetorno;
+Begin
+  Result.Resultado:= False;
+
+  CriarTabela_sis_parametros_se_necessario;
+
+  //
+  Result:= TSVPE_VazioOuDir('ACNFE_HOME', vACNFE_HOME);
+  if Not Result.Resultado then
+    Exit;
+  //
+
+  vACNFE_HOME:= IncludeTrailingPathDelimiter(vACNFE_HOME);
+  vACNFE_EXE:= vACNFE_HOME + 'acnfe.exe';
+
+  Result:= TSVPE_VazioOuArq('ACNFE_EXE', vACNFE_EXE);
+  if Not Result.Resultado then
+    Exit;
+
+  //
+  Result:= TSVPE_VazioOuDir('ACNFE_TMP', vACNFE_TMP);
+  if Not Result.Resultado then
+    Exit;
+  //
+
+  Result:= TSVPE_VazioOuDir('ACNFE_PDF', vACNFE_PDF);
+  if Not Result.Resultado then
+    Exit;
+  //
+
+  Result:= TSVPE_VazioOuDir('ACNFE_XML', vACNFE_XML);
+  if Not Result.Resultado then
+    Exit;
+  //
+
+  {
+  Result:= TSVPE_VazioOuDir('ACNFE_OUT', vACNFE_OUT);
+  if Not Result.Resultado then
+    Exit;
+  //
+  }
+
+End;
+
+Function gerarNodosFilhosDeSelect(
+  pNodoAvo: IXMLNode;
+  pNomePai, pSQL: String;
+  pImprimirConteudoTagSeVazia: Boolean = True;
+  pds: TDataSet = Nil
+  ): TRetorno;
+Var
+  // vds: TDataSet;
+  vi: Integer;
+  vf: TField;
+  vNodoPai: IXMLNode;
+  vCriouTabela: Boolean;
+Begin
+
+  vCriouTabela:= pds = Nil;
+
+  if vCriouTabela then
+    vi:= uDados.uDM.FDC.ExecSQL(pSQL, pds);
+
+  if Not pds.IsEmpty then
+  Begin
+    vNodoPai:= pNodoAvo.AddChild(pNomePai);
+    for vf in pds.Fields Do
+      if
+     Not(vf.AsString.Trim.IsEmpty) or
+        (vf.AsString.Trim.IsEmpty and pImprimirConteudoTagSeVazia) then
+        vNodoPai.AddChild(vf.FieldName).NodeValue:= vf.AsString;
+  End;
+
+  if vCriouTabela then
+  Begin
+    pds.Close;
+    pds.Free;
+    pds:= Nil;
+  End;
+
+End;
+
+Function geraride(pSis_Pessoa_id, pCom_Pedido_Numero: Integer; pinfNFe: IXMLNode): TRetorno;
+Var
+  sSQL: String;
+Begin
+
+  sSQL:=
+    'select ' + #13#10 +
+    '  substring(ibgecidade, 1, 2) "cUF", ' + #13#10 +
+    '  floor(rand()*100000000) "cNF", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''natOp'') "natOp", ' + #13#10 +
+    '  65 "mod", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''serie'') "serie",  ' + #13#10 +
+    '  ( ' + #13#10 +
+    '    select ' + #13#10 +
+    '      ifnull( ' + #13#10 +
+    '        max(NrNFCe), ' + #13#10 +
+    '        (select valor from sis_parametros where nome = ''ultnNF'') ' + #13#10 +
+    '    ) + 1 ' + #13#10 +
+    '    from com_pedido ' + #13#10 +
+    '    where NrNFCe is not null ' + #13#10 +
+    '  ) "nNF", ' + #13#10 +
+    '  CONCAT(CURDATE(), ''T'', CURTIME(), ifnull((select valor from sis_parametros where nome = ''TZD''), '''')) "dhEmi", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''tpNF'') "tpNF", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''idDest'') "idDest",' + #13#10 +
+    '  ibgecidade "cMunFG", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''tpImp'') "tpImp", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''tpEmis'') "tpEmis", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''tpAmb'') "tpAmb", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''finNFe'') "finNFe", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''indFinal'') "indFinal", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''indPres'') "indPres", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''indIntermed'') "indIntermed", ' + #13#10 +
+    '  (select valor from sis_parametros where nome = ''procEmi'') "procEmi" ' + #13#10 +
+    'from sis_pessoa ' + #13#10 +
+    'where id = (select valor from sis_parametros where nome = ''emissor_sis_pessoa_id'')';
+
+  Result:= gerarNodosFilhosDeSelect(pinfNFe, 'ide', sSQL);
+
+End;
+
+
+Function gerarenderEmit(pemit: IXMLNode): TRetorno;
+Var
+  sSQL: String;
+Begin
+
+  sSQL:=
+    'select ' +
+    '  logradouro "xLgr", ' +
+    '  nro, ' +
+    '  complemento "xCompl", ' +
+    '  bairro "xBairro", ' +
+    '  ibgecidade "cMun", ' +
+    '  cidade "xMun", ' +
+    '  uf "UF", ' +
+    '  cep "CEP", ' +
+    '  telefone "fone" ' +
+    'from sis_pessoa ' +
+    'where id = (select valor from sis_parametros where nome = ''emissor_sis_pessoa_id'') ';
+
+  Result:= gerarNodosFilhosDeSelect(pemit, 'enderemit', sSQL);
+
+End;
+
+
+Function geraremit(pinfNFe: IXMLNode): TRetorno;
+Var
+  nemit: IXMLNode;
+  sSQL: String;
+  vds: TDataSet;
+  vi: Integer;
+Begin
+
+  nemit:= pinfNFe.AddChild('emit');
+
+  sSQL:=
+    'select ' +
+    '  cnpj "CNPJ", ' +
+    '  nome "xNome", ' +
+    '  fantasia "xFant", ' +
+    '  IE "IE", ' +
+    '  (select valor from sis_parametros where nome = ''CRT'') "CRT" ' +
+    'from sis_pessoa ' +
+    'where id = (select valor from sis_parametros where nome = ''emissor_sis_pessoa_id'') ';
+
+  vi:= uDados.uDM.FDC.ExecSQL(sSQL, vds);
+
+  nemit.AddChild('CNPJ').NodeValue := vds.FieldByName('CNPJ').AsString;
+  nemit.AddChild('xNome').NodeValue:= vds.FieldByName('xNome').AsString;
+  nemit.AddChild('xFant').NodeValue:= vds.FieldByName('xFant').AsString;
+
+  Result:= gerarenderEmit( nemit );
+  if Not Result.Resultado then
+    Exit;
+
+End;
+
+
+Function gerardest(pCom_Pedido_Numero: Integer; pninfNFe: IXMLNode): TRetorno;
+Var
+  sSQL: String;
+Begin
+
+  sSQL:=
+    'select ' +
+    '  ( ' +
+    '    CASE WHEN CHAR_LENGTH(IFNULL(CPF_CNPJ, '''')) <= 11 THEN ' +
+    '      IFNULL(CPF_CNPJ, '''') ' +
+    '  ELSE ' +
+    '      '''' ' +
+    '    END ' +
+    '  ) "CPF", ' +
+    '  ( ' +
+    '    CASE WHEN CHAR_LENGTH(IFNULL(CPF_CNPJ, '''')) > 11 THEN ' +
+    '      IFNULL(CPF_CNPJ, '''') ' +
+    '  ELSE ' +
+    '      '''' ' +
+    '    END ' +
+    '  ) "CNPJ" ' +
+    'from com_pedido ' +
+    'where CPF_CNPJ is not null ' +
+    '  and numero = ' + pCom_Pedido_Numero.ToString;
+
+
+  Result:= gerarNodosFilhosDeSelect(pninfNFe, 'dest', sSQL, False);
+
+End;
+
+
+Function gerardet(pCom_Pedido_Numero: Integer; pninfNFe: IXMLNode): TRetorno;
+Var
+  sSQL: String;
+  vds: TDataSet;
+  vi: Integer;
+  pdet: IXMLNode;
+Begin
+
+  sSQL:=
+    'select ' +
+    '  (@row_number := @row_number + 1) "nItem", ' +
+    '  i.* ' +
+    'from ' +
+    '(' +
+    'select ' +
+    '   CodProd "cProd", ' +
+    '   ''SEM GTIN'' as "cEAN", ' +
+    '   Descricao "xProd", ' +
+    '   NCM "NCM", ' +
+    '   CFOP "CFOP", ' +
+    '   Unidade "uCom", ' +
+    '   Quant "qCom", ' +
+    '   VlrUnitario "vUnCom", ' +
+    '   VlrTotal "vProd", ' +
+    '   ''SEM GTIN'' as "cEANTrib", ' +
+    '   Unidade "uTrib",' +
+    '   Quant "qTrib", ' +
+    '   VlrUnitario "vUnTrib", ' +
+    '   1 as "indTot" ' +
+    'from com_pedidoitem ' +
+    'left join com_itens on com_pedidoitem.CodProd = com_itens.codigo ' +
+    'where numero = ' + pCom_Pedido_Numero.ToString + ' ' +
+    'order by NrLcto ' +
+    ') as i ' +
+    'inner join (SELECT @row_number := 0) rn on 1=1 ';
+
+  vi:= uDados.uDM.FDC.ExecSQL(sSQL, vds);
+
+  if vds.IsEmpty then
+  Begin
+    Result.Resultado:= False;
+    Result.Mensagem:= 'Não foram encontrados itens neste pedido!';
+    Exit;
+  End;
+
+  vds.First;
+  while Not vds.Eof do
+  Begin
+    pdet:= pninfNFe.AddChild('det');
+    pdet.Attributes['nItem']:= vds.FieldByName('nItem').AsString;
+    Result:= gerarNodosFilhosDeSelect(pdet, 'prod', EmptyStr, True, vds);
+
+    if Not Result.Resultado then
+      Break;
+
+    vds.Next;
+  End;
+
+  vds.Close;
+  vds.Free;
+  vds:= Nil;
+
+
+End;
+
+Function gerarInfNFe(pSis_Pessoa_id, pCom_Pedido_Numero: Integer; pNFe: IXMLNode): TRetorno;
+Var
+  ninfNFe: IXMLNode;
+Begin
+
+  ninfNFe:= pNFe.AddChild('infNFe');
+
+  Result:= geraride(pSis_Pessoa_id, pCom_Pedido_Numero, ninfNFe);
+  if Not Result.Resultado then
+    Exit;
+
+  Result:= geraremit(ninfNFe);
+  if Not Result.Resultado then
+    Exit;
+
+  Result:= gerardest(pCom_Pedido_Numero, ninfNFe);
+  if Not Result.Resultado then
+    Exit;
+
+  Result:= gerardet(pCom_Pedido_Numero, ninfNFe);
+  if Not Result.Resultado then
+    Exit;
+
+
+
+
+End;
+
+
+Function gerarNFe(pSis_Pessoa_id, pCom_Pedido_Numero: Integer): TRetorno;
+Var
+  nNFe: IXMLNode;
+Begin
+
+  if vxmld = Nil then
+    vxmld:= TXMLDocument.Create(uDM);
+
+  vxmld.XML.Text:= EmptyStr;
+  vxmld.Active  := True;
+
+  nNFe:= vxmld.AddChild('NFe');
+
+  Result:= gerarInfNFe(pSis_Pessoa_id, pCom_Pedido_Numero, nNFe);
+
+
+
+End;
+
+
+
+Function getGerarArqXML(pSis_Pessoa_id, pCom_Pedido_Numero: Integer): TRetorno;
+Begin
+  Result:= gerarNFe(pSis_Pessoa_id, pCom_Pedido_Numero);
+
+  if Not Result.Resultado then
+    Exit;
+
+  vxmld.XML.SaveToFile( IncludeTrailingPathDelimiter(vACNFE_TMP) + pCom_Pedido_Numero.ToString + '.xml'  );
+  vxmld.Active:= False;
+
+
+End;
+
+Function EmitirNFCeDePV(pSis_Pessoa_id, pCom_Pedido_Numero: Integer): TRetorno;
+Begin
+  Result:= ValidarParametrosNFCe;
+  if Not Result.Resultado then
+    Exit;
+
+  Result:= getGerarArqXML(pSis_Pessoa_id, pCom_Pedido_Numero);
+  if Not Result.Resultado then
+    Exit;
+
+
+End;
+
+end.
---
+++ vcl/uTurno.dfm
@@ -0,0 +1,187 @@
+object FuTurno: TFuTurno
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu]
+  Caption = 'T u r n o'
+  ClientHeight = 371
+  ClientWidth = 384
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -16
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  Position = poDesigned
+  OnActivate = FormActivate
+  OnCreate = FormCreate
+  TextHeight = 19
+  object LabRotina: TLabel
+    Left = 0
+    Top = 0
+    Width = 384
+    Height = 25
+    Align = alTop
+    Alignment = taCenter
+    Caption = 'Abertura/Encerramento'
+    Color = clNavy
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWhite
+    Font.Height = -21
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentColor = False
+    ParentFont = False
+    Transparent = False
+    ExplicitWidth = 251
+  end
+  object PanTurno: TPanel
+    Left = 0
+    Top = 25
+    Width = 384
+    Height = 271
+    Align = alClient
+    TabOrder = 0
+    ExplicitHeight = 161
+    DesignSize = (
+      384
+      271)
+    object Label1: TLabel
+      Left = 56
+      Top = 28
+      Width = 88
+      Height = 19
+      Alignment = taRightJustify
+      Caption = 'Turno atual:'
+    end
+    object dbTurno: TDBText
+      Left = 148
+      Top = 24
+      Width = 79
+      Height = 25
+      AutoSize = True
+      DataField = 'NrTurno'
+      DataSource = uDM.DSTurnos
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -21
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+    object LabInicio: TLabel
+      Left = 59
+      Top = 84
+      Width = 45
+      Height = 19
+      Alignment = taRightJustify
+      Caption = 'In'#237'cio:'
+    end
+    object LabFim: TLabel
+      Left = 72
+      Top = 148
+      Width = 32
+      Height = 19
+      Alignment = taRightJustify
+      Caption = 'Fim:'
+    end
+    object dbTxtStatus: TDBText
+      Left = 285
+      Top = 232
+      Width = 84
+      Height = 19
+      Alignment = taRightJustify
+      Anchors = [akLeft, akBottom]
+      AutoSize = True
+      DataField = 'Status'
+      DataSource = uDM.DSTurnos
+    end
+    object edInicio: TDBEdit
+      Left = 110
+      Top = 81
+      Width = 172
+      Height = 27
+      DataField = 'DtHrInicio'
+      DataSource = uDM.DSTurnos
+      TabOrder = 0
+      OnKeyDown = edInicioKeyDown
+    end
+    object edFinal: TDBEdit
+      Left = 110
+      Top = 145
+      Width = 172
+      Height = 27
+      DataField = 'DtHrFim'
+      DataSource = uDM.DSTurnos
+      Enabled = False
+      TabOrder = 1
+      OnKeyDown = edFinalKeyDown
+    end
+  end
+  object PanControle: TPanel
+    Left = 0
+    Top = 296
+    Width = 384
+    Height = 75
+    Align = alBottom
+    TabOrder = 1
+    ExplicitTop = 186
+    object btOk: TBitBtn
+      Left = 14
+      Top = 8
+      Width = 169
+      Height = 57
+      Caption = '&Ok'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -16
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      Glyph.Data = {
+        76010000424D7601000000000000760000002800000020000000100000000100
+        04000000000000010000120B0000120B00001000000000000000000000000000
+        800000800000008080008000000080008000808000007F7F7F00BFBFBF000000
+        FF0000FF000000FFFF00FF000000FF00FF00FFFF0000FFFFFF00555555555555
+        555555555555555555555555555555555555555555FF55555555555559055555
+        55555555577FF5555555555599905555555555557777F5555555555599905555
+        555555557777FF5555555559999905555555555777777F555555559999990555
+        5555557777777FF5555557990599905555555777757777F55555790555599055
+        55557775555777FF5555555555599905555555555557777F5555555555559905
+        555555555555777FF5555555555559905555555555555777FF55555555555579
+        05555555555555777FF5555555555557905555555555555777FF555555555555
+        5990555555555555577755555555555555555555555555555555}
+      NumGlyphs = 2
+      ParentFont = False
+      TabOrder = 0
+      OnClick = btOkClick
+    end
+    object btCancel: TBitBtn
+      Left = 200
+      Top = 8
+      Width = 169
+      Height = 57
+      Caption = '&Not Ok'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -16
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      Glyph.Data = {
+        76010000424D7601000000000000760000002800000020000000100000000100
+        04000000000000010000120B0000120B00001000000000000000000000000000
+        8000008000000080800080000000800080008080000080808000C0C0C0000000
+        FF0000FF000000FFFF00FF000000FF00FF00FFFF0000FFFFFF00666666666666
+        6666666FFFF6666666FF6619996666666996667888F66666688F666199966666
+        99166667888F6666887F66661996666991666666788F666887F6666661996699
+        166666666788FF887F666666619999916666666667888887F666666666199916
+        666666666678887F6666666666999966666666666F8888F66666666669991996
+        66666666F888788F66666666999161996666666F88876788F666666999166619
+        966666F8887666788F6666999166666996666688876666678F66669916666661
+        996666887666666788F66691666666661996668766666666788F666666666666
+        6199666666666666678866666666666666666666666666666666}
+      NumGlyphs = 2
+      ParentFont = False
+      TabOrder = 1
+      OnClick = btCancelClick
+    end
+  end
+end
